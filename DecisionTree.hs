7f6299c2edec32ea91840aad114d76e5005534f8
min2028 <ronaldaungkhantmin@gmail.com>
2023-02-27 16:21:20 -0800

    ComputeGini

--- DecisionTree.hs
+++ DecisionTree.hs
@@ -7,8 +7,12 @@ import qualified Data.Map as Map
 data DTtree a b = DTNode ((a,b) -> Bool) (DTtree a b) (DTtree a b)
                 | LeafNode b
 
--- instance (show b) => show (DTtree a b) where
---     show LeafNode b = b
+instance (Show a, Show b) => Show (DTtree a b) where
+    show (LeafNode b) = show b
+    show (DTNode p lt rt) = "<" ++ showFunction p ++ ": " ++ show lt ++ " | " ++ show rt ++ ">"
+
+showFunction :: ((a, b) -> Bool) -> String
+showFunction p = "function"
 
 isThisLeafPure :: Eq b => [(a, b)] -> Bool
 isThisLeafPure [] = True
@@ -25,7 +29,7 @@ isThisLeafPure ((a,b):t) = foldr (\ (x, y) z -> (y == b) && z) True ((a,b):t)
 -- isThisLeafPure [("h", "yes"), ("i", "yes"), ("j", "yes")]
 
 
-buildTree :: (Eq a, Ord b) => Integer -> [(a,b)] -> DTtree a b 
+buildTree :: (Eq a, Ord a, Ord b) => Integer -> [(a,b)] -> DTtree a b 
 buildTree maxDepth trainingData
     -- if we reached max depth or if the leaf is pure, we can just return LeafNode, 
     -- otherwise create branch for left and right subtree
@@ -44,8 +48,34 @@ mostCommonClass classes =
     in maxClass
 
 -- stub
-nextFeatureToSplit :: [(a, b)] -> (a, b)
-nextFeatureToSplit dataPoints = head dataPoints
+nextFeatureToSplit :: (Ord a, Ord b) => [(a, b)] -> (a, b)
+nextFeatureToSplit trainingData =
+    let (features, values) = unzip trainingData
+        featureSet = nub $ sort features
+        valueSet = nub $ sort values
+        splitValues = [((f,v), computeGiniIndex f v trainingData) | f <- featureSet, v <- valueSet]
+        ((bestFeature, bestValue), _) = minimumBy (comparing snd) splitValues
+    in (bestFeature, bestValue)
+
+computeGiniIndex :: (Ord a, Ord b) => a -> b -> [(a, b)] -> Double
+computeGiniIndex feature value trainingData =
+    let (yesData, noData) = splitData feature value trainingData
+        totalCount = fromIntegral $ length trainingData
+        yesCount = fromIntegral $ length yesData
+        noCount = fromIntegral $ length noData
+        yesFraction = yesCount / totalCount
+        noFraction = noCount / totalCount
+        yesGini = computeFractionGini yesData
+        noGini = computeFractionGini noData
+    in (yesFraction * yesGini) + (noFraction * noGini)
+
+computeFractionGini :: (Ord a, Ord b) => [(a, b)] -> Double
+computeFractionGini dataPoints =
+    let classCounts = Map.fromListWith (+) [(c, 1) | (_, c) <- dataPoints]
+        totalCount = fromIntegral $ length dataPoints
+        sumSquaredFractions = sum [fraction * fraction | count <- Map.elems classCounts,
+                                                          let fraction = fromIntegral count / totalCount]
+    in 1 - sumSquaredFractions
 
 splitData :: (Eq a, Ord b) => a -> b -> [(a, b)] -> ([(a, b)], [(a, b)])
 splitData feature value = partition (\(dfeature, dvalue) -> (== feature) dfeature && (<= value) dvalue)
